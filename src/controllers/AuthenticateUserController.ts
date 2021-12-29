df75ef3bd017b496a069af05fdd24c80687b36e9
Jeremias Marinho <jeremias10@msn.com>
2021-12-29 07:04:03 -0300

    Implantação do prisma e configuração de autenticação com gihub

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -1,13 +1,14 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import { AutheticateUserService } from "../services/AuthenticateUserService";
-
-
 class AuthenticateUserController {
-  async handle(request: Request, response:Response) {
+  async handle(request: Request, response: Response) {
+    const { code } = request.body;
 
     const service = new AutheticateUserService();
-
+    const result = await service.execute(code);
+  
+    return response.json(result);
   }
 }
 
-export {AuthenticateUserController};
+export { AuthenticateUserController };
